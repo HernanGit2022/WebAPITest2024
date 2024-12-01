0c7c5acd3b6b091b487bb50175e4f98f87b6b44b
HernanHerzum <hernan.alvarez@herzum.com>
2024-12-01 14:13:43 -0500

    Created AuditBase and Country Entities

---
+++ WebAPITest/DAL/Entities/AuditBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPITest.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key] //PK
+        [Required] //Significa que este campo es obligatorio
+        public virtual Guid ID { get; set; } //Esta será el PK de todas las tablas
+        public virtual DateTime? CreatedDate { get; set; } //Para guardar todo registro nuevo con su fecha
+        public virtual DateTime? ModifiedDate { get; set; } //Para guardar todo registro que se modificó con su fecha
+    }
+}
---
+++ WebAPITest/DAL/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPITest.DAL.Entities
+{
+    public class Country : AuditBase
+    {
+        [Display(Name = "País")] //Para identificar el nombre más fácil
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //Longitud máximo
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //Campo obligatorio
+        public string Name { get; set; }
+    }
+}
